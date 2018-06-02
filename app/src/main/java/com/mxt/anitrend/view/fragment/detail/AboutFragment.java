a0b2d4a74480d9ebb7b2e17f455fc0f612f6078b
Maxwell <mxt.developer@gmail.com>
2018-06-02 11:09:39 +0200

    Updated About Activity
    - Includes legal links to the application T&Cs

--- app/src/main/java/com/mxt/anitrend/view/fragment/detail/AboutFragment.java
+++ app/src/main/java/com/mxt/anitrend/view/fragment/detail/AboutFragment.java
@@ -1,16 +1,20 @@
 package com.mxt.anitrend.view.fragment.detail;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.mxt.anitrend.BuildConfig;
 import com.mxt.anitrend.R;
 import com.mxt.anitrend.base.custom.fragment.FragmentBase;
 import com.mxt.anitrend.presenter.base.BasePresenter;
+import com.mxt.anitrend.util.NotifyUtil;
 
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
@@ -32,14 +36,22 @@ public class AboutFragment extends FragmentBase<Void, BasePresenter, Void> {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return new AboutPage(getActivity())
+        AboutPage aboutPage = new AboutPage(getActivity())
                 .setImage(R.mipmap.ic_launcher)
-                .addGroup("About AniTrend")
+                .addGroup("General Information")
                 .setDescription(getString(R.string.app_description))
                 .addItem(new Element().setTitle(String.format("Version %s", BuildConfig.VERSION_NAME)))
                 .addPlayStore("com.mxt.anitrend")
                 .addTwitter("anitrend_app")
-                .create();
+                .addGroup("Additional Information")
+                .addGitHub("AniTrend")
+                .addWebsite("https://anitrend.co")
+                .addGroup("Legal Information")
+                .addItem(new Element().setTitle("Terms & Conditions").setIconDrawable(R.drawable.ic_privacy_grey_600_24dp)
+                        .setIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/AniTrend/anitrend-app/blob/develop/TERMS_OF_SERVICE.md"))))
+                .addItem(new Element().setTitle("Code Of Conduct").setIconDrawable(R.drawable.ic_privacy_grey_600_24dp)
+                        .setIntent(new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/AniTrend/anitrend-app/blob/develop/CODE_OF_CONDUCT.md"))));
+        return aboutPage.create();
     }
 
     @Override
