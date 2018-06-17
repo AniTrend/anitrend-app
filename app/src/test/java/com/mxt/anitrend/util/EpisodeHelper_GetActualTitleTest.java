fd23d41ccea199fb614dda45e0c945a7e9459478
Ali Salah <rated.ali.7@gmail.com>
2018-06-17 12:48:16 +0200

    Add tests for the EpisodeHelper utilities class

---
+++ app/src/test/java/com/mxt/anitrend/util/EpisodeHelper_GetActualTitleTest.java
@@ -0,0 +1,36 @@
+package com.mxt.anitrend.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(Parameterized.class)
+public class EpisodeHelper_GetActualTitleTest {
+
+    @Parameterized.Parameters
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"Boku no Hero Academia - Episode 23", "Boku no Hero Academia"},
+                {"Haikyuu Season 3", "Haikyuu"},
+                {"Boku no Hero Academia Season 2 - Episode 19", "Boku no Hero Academia"}
+        });
+    }
+
+    @Parameterized.Parameter(0)
+    public String inputTitle;
+
+    @Parameterized.Parameter(1)
+    public String actualTitle;
+
+
+    @Test
+    public void getActualTile() {
+        assertThat(EpisodeHelper.getActualTile(inputTitle), equalTo(actualTitle));
+    }
+
+}
