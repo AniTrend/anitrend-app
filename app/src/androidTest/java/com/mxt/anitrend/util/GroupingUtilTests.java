0cf6fa05ed208802cd4a4b926e2440092d926224
Ali Salah <rated.ali.7@gmail.com>
2018-06-20 01:03:19 +0200

    Add tests for GroupingUtil.groupActorMediaEdge and GroupingUtil.groupMediaByRelationType

--- app/src/androidTest/java/com/mxt/anitrend/util/GroupingUtilTests.java
+++ app/src/androidTest/java/com/mxt/anitrend/util/GroupingUtilTests.java
@@ -1,5 +1,8 @@
 package com.mxt.anitrend.util;
 
+import com.mxt.anitrend.model.entity.anilist.edge.CharacterEdge;
+import com.mxt.anitrend.model.entity.anilist.edge.MediaEdge;
+import com.mxt.anitrend.model.entity.base.CharacterBase;
 import com.mxt.anitrend.model.entity.base.MediaBase;
 import com.mxt.anitrend.model.entity.base.StaffBase;
 import com.mxt.anitrend.model.entity.group.RecyclerHeaderItem;
@@ -18,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,13 +30,24 @@ import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class GroupingUtilTests {
 
+    // all media relation types
+    private static final String[] relationTypes = {
+            KeyUtil.ADAPTATION, KeyUtil.PREQUEL, KeyUtil.SEQUEL, KeyUtil.PARENT,
+            KeyUtil.SIDE_STORY, KeyUtil.CHARACTER, KeyUtil.SUMMARY, KeyUtil.ALTERNATIVE,
+            KeyUtil.SPIN_OFF
+    };
+    private static final String[] characterRoles = {
+            KeyUtil.MAIN, KeyUtil.SUPPORTING, KeyUtil.BACKGROUND
+    };
+    private static final String[] languages = {"ENGLISH", "JAPANESE"};
 
     // a sorted list of MediaBase objects of all media formats
-    private List<MediaBase> allMedia =
+    private List<MediaBase> mediaOfAllFormats =
             Stream.of(KeyUtil.MediaFormat)
                     .filter(Objects::nonNull)
                     .map(format -> {
@@ -44,11 +59,11 @@ public class GroupingUtilTests {
                     .collect(Collectors.toList());
 
     private Map<String, List<MediaBase>> mediaFormatMap =
-            allMedia.stream().collect(Collectors.groupingBy(MediaBase::getFormat));
+            mediaOfAllFormats.stream().collect(Collectors.groupingBy(MediaBase::getFormat));
 
     // a sorted list of StaffBase objects of different languages
-    private List<StaffBase> allStaff =
-            Stream.of("ENGLISH", "JAPANESE")
+    private List<StaffBase> staffOfAllLanguages =
+            Stream.of(languages)
                     .filter(Objects::nonNull)
                     .sorted()
                     .map(language -> {
@@ -58,10 +73,63 @@ public class GroupingUtilTests {
                     }).collect(Collectors.toList());
 
     private Map<String, List<StaffBase>> staffLanguageMap =
-            allStaff.stream().collect(Collectors.groupingBy(StaffBase::getLanguage));
+            staffOfAllLanguages.stream().collect(Collectors.groupingBy(StaffBase::getLanguage));
 
+    // a sorted list of MediaEdge objects of all media relations
+    private List<MediaEdge> mediaOfAllRelations =
+            Stream.of(relationTypes)
+                    .sorted()
+                    .flatMap(relation -> {
+                        List<MediaEdge> edges = Arrays.asList(
+                                mock(MediaEdge.class),
+                                mock(MediaEdge.class));
+                        edges.forEach(edge -> {
+                            when(edge.getRelationType()).thenReturn(relation);
+                            MediaBase media = mock(MediaBase.class);
+                            when(edge.getNode()).thenReturn(media);
+                        });
+                        return edges.stream();
+                    }).collect(Collectors.toList());
+
+    private Map<String, List<MediaBase>> mediaRelationMap =
+            mediaOfAllRelations.stream()
+                    .collect(Collectors.groupingBy(MediaEdge::getRelationType))
+                    .entrySet()
+                    .stream()
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            entry -> entry.getValue()
+                                    .stream()
+                                    .map(MediaEdge::getNode)
+                                    .collect(Collectors.toList())));
+
+    // a sorted list of CharacterEdge objects of all character roles
+    private List<CharacterEdge> charactersOfAllRoles =
+            Stream.of(characterRoles)
+                    .sorted()
+                    .map(role -> {
+                        CharacterEdge edge = mock(CharacterEdge.class);
+                        when(edge.getRole()).thenReturn(role);
+                        CharacterBase character = mock(CharacterBase.class);
+                        when(edge.getNode()).thenReturn(character);
+                        return edge;
+                    })
+                    .collect(Collectors.toList());
+
+    private Map<String, List<CharacterBase>> characterRoleMap =
+            charactersOfAllRoles.stream()
+                    .collect(Collectors.groupingBy(CharacterEdge::getRole))
+                    .entrySet()
+                    .stream()
+                    .collect(Collectors.toMap(
+                            Map.Entry::getKey,
+                            entry -> entry.getValue()
+                                    .stream()
+                                    .map(CharacterEdge::getNode)
+                                    .collect(Collectors.toList())));
 
     //region groupMediaByFormat
+
     @Test
     public void groupMediaByFormat_ifTheMediaListIsEmpty_shouldReturnAnEmptyList() {
         assertThat(GroupingUtil.groupMediaByFormat(Collections.emptyList(), null), empty());
@@ -83,7 +151,7 @@ public class GroupingUtilTests {
 
 
         List<RecyclerItem> results =
-                GroupingUtil.groupMediaByFormat(allMedia, null);
+                GroupingUtil.groupMediaByFormat(mediaOfAllFormats, null);
 
         assertThat(results, hasSize(required.size()));
         assertThat(results, containsItemsOf(required));
@@ -118,7 +186,7 @@ public class GroupingUtilTests {
                         .collect(Collectors.toList());
 
         List<RecyclerItem> results =
-                GroupingUtil.groupMediaByFormat(allMedia, existingItems);
+                GroupingUtil.groupMediaByFormat(mediaOfAllFormats, existingItems);
 
         assertThat(results, hasSize(required.size()));
         assertThat(results, containsItemsOf(required));
@@ -147,7 +215,7 @@ public class GroupingUtilTests {
 
 
         List<RecyclerItem> results =
-                GroupingUtil.groupStaffByLanguage(allStaff, null);
+                GroupingUtil.groupStaffByLanguage(staffOfAllLanguages, null);
 
         assertThat(results, hasSize(required.size()));
         assertThat(results, containsItemsOf(required));
@@ -182,7 +250,7 @@ public class GroupingUtilTests {
                         .collect(Collectors.toList());
 
         List<RecyclerItem> results =
-                GroupingUtil.groupStaffByLanguage(allStaff, existingItems);
+                GroupingUtil.groupStaffByLanguage(staffOfAllLanguages, existingItems);
 
         assertThat(results, hasSize(required.size()));
         assertThat(results, containsItemsOf(required));
@@ -195,17 +263,80 @@ public class GroupingUtilTests {
         assertThat(GroupingUtil.groupActorMediaEdge(Collections.emptyList()), empty());
     }
 
+    @Test
+    public void groupActorMediaEdge_shouldReturnTheMediaAsAHeaderFollowedByVoiceActors() {
+        List<MediaEdge> mediaEdges =
+                Stream.of(characterRoles)
+                        .map(role -> {
+                            MediaEdge edge = mock(MediaEdge.class);
+                            when(edge.getCharacterRole()).thenReturn(role);
 
+                            StaffBase va1 = mock(StaffBase.class);
+                            StaffBase va2 = mock(StaffBase.class);
+                            when(edge.getVoiceActors()).thenReturn(Arrays.asList(va1, va2));
 
+                            MediaBase media = mock(MediaBase.class);
+                            when(edge.getNode()).thenReturn(media);
+
+                            return edge;
+                        }).collect(Collectors.toList());
+
+        List<RecyclerItem> required =
+                mediaEdges.stream()
+                        .flatMap(edge -> {
+                            List<RecyclerItem> items = new ArrayList<>();
+                            items.add(edge.getNode());
+                            items.addAll(edge.getVoiceActors());
+                            return items.stream();
+                        }).collect(Collectors.toList());
+
+        List<RecyclerItem> result = GroupingUtil.groupActorMediaEdge(mediaEdges);
+
+        assertThat(result, hasSize(required.size()));
+        assertThat(result, containsItemsOf(required));
+
+        List<MediaBase> media = result.stream()
+                .filter(item -> item instanceof MediaBase)
+                .map(item -> (MediaBase) item)
+                .collect(Collectors.toList());
+        // verify that the content type is set as header for media items
+
+        // and the subgroup title is set to the character role
+        for (int i = 0; i < media.size(); i++) {
+            verify(media.get(i)).setSubGroupTitle(characterRoles[i]);
+            verify(media.get(i)).setContentType(KeyUtil.RECYCLER_TYPE_HEADER);
+        }
+
+    }
     //endregion
 
+    //region groupMediaByRelationType
     @Test
-    public void groupMediaByRelationType() {
+    public void groupMediaByRelationType_ifEdgeListIsEmpty_shouldReturnAnEmptyList() {
+        assertThat(GroupingUtil.groupMediaByRelationType(Collections.emptyList()), empty());
     }
 
+    @Test
+    public void groupMediaByRelationType_shouldReturnAHeaderForEachRelationFollowedByMedia() {
+        List<RecyclerItem> required =
+                mediaRelationMap.keySet().stream()
+                        .sorted()
+                        .flatMap(getRecyclerItemsMapperForMap(mediaRelationMap))
+                        .collect(Collectors.toList());
+
+        List<RecyclerItem> results =
+                GroupingUtil.groupMediaByRelationType(mediaOfAllRelations);
+
+        assertThat(results, hasSize(required.size()));
+        assertThat(results, containsItemsOf(required));
+    }
+    //endregion
+
+    //region groupCharactersByRole
     @Test
     public void groupCharactersByRole() {
     }
+    //endregion
 
     @Test
     public void groupStaffByRole() {
@@ -228,15 +359,23 @@ public class GroupingUtilTests {
         return getRecyclerItemsMapperForMap(map, format -> true);
     }
 
+    /**
+     * Make a mapper that transforms keys from a given {@link java.util.Map} to a stream of recycler items
+     * this stream conditionally contains a {@link RecyclerHeaderItem} for each key followed by the associated list of items
+     * from the map
+     *
+     * @param map           specifies the items associated with each key
+     * @param includeHeader decides if a header is included in the stream for a given key
+     */
     private static <T extends RecyclerItem> Function<String, Stream<RecyclerItem>> getRecyclerItemsMapperForMap(Map<String, List<T>> map,
-                                                                                                                Function<String, Boolean> includeHeader) {
-        return format -> {
-            List<RecyclerItem> formatItems = new ArrayList<>();
-            if (includeHeader.apply(format)) {
-                formatItems.add(new RecyclerHeaderItem(format, map.get(format).size()));
+                                                                                                                Predicate<String> includeHeader) {
+        return key -> {
+            List<RecyclerItem> items = new ArrayList<>();
+            if (includeHeader.test(key)) {
+                items.add(new RecyclerHeaderItem(key, map.get(key).size()));
             }
-            formatItems.addAll(map.get(format));
-            return formatItems.stream();
+            items.addAll(map.get(key));
+            return items.stream();
         };
     }
 
