9ad2b595c88119f9f031ed81a38fba2070a1f839
Maxwell <mxt.developer@gmail.com>
2018-09-13 11:36:13 +0200

    API Rate Limit Message Handling
    - Human readable API rate limit exception display message for users

--- app/src/main/java/com/mxt/anitrend/util/ErrorUtil.java
+++ app/src/main/java/com/mxt/anitrend/util/ErrorUtil.java
@@ -13,6 +13,7 @@ import com.mxt.anitrend.model.entity.container.body.GraphContainer;
 import java.lang.reflect.Type;
 import java.util.List;
 
+import okhttp3.Headers;
 import okhttp3.ResponseBody;
 import retrofit2.Response;
 
@@ -23,7 +24,12 @@ import retrofit2.Response;
 
 public class ErrorUtil {
 
+    private static final int HTTP_LIMIT_REACHED = 429;
+
     private static final String TAG = "ErrorUtil";
+    private static final String Retry_After = "Retry-After";
+    private static final String RateLimit_Limit = "X-RateLimit-Limit";
+    private static final String RateLimit_Remaining = "X-RateLimit-Remaining";
 
     /**
      * Converts the response error response into an object.
@@ -34,11 +40,18 @@ public class ErrorUtil {
     public static @NonNull String getError(@Nullable Response response) {
         try {
             if(response != null) {
+                Headers headers = response.headers();
                 ResponseBody responseBody = response.errorBody();
                 String message, error;
-                if (responseBody != null && !TextUtils.isEmpty(message = responseBody.string()))
-                    if(!TextUtils.isEmpty(error = getGraphQLError(message)))
-                        return error;
+                if (response.code() != HTTP_LIMIT_REACHED) {
+                    if (responseBody != null && !TextUtils.isEmpty(message = responseBody.string()))
+                        if (!TextUtils.isEmpty(error = getGraphQLError(message)))
+                            return error;
+                } else {
+                    error = String.format("%s of %s requests remaining, please retry after %s seconds",
+                            headers.get(RateLimit_Remaining), headers.get(RateLimit_Limit), headers.get(Retry_After));
+                    return error;
+                }
             }
         } catch (Exception ex) {
             ex.printStackTrace();
