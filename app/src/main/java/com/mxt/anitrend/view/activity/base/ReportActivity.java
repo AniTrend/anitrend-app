6d57fdea840da90c9bdf0162e0a7aedba106fcf0
Mehul Mittal <mehulmittal2010@gmail.com>
2019-01-14 22:31:27 +0530

    Add functionality to save logcat

--- app/src/main/java/com/mxt/anitrend/view/activity/base/ReportActivity.java
+++ app/src/main/java/com/mxt/anitrend/view/activity/base/ReportActivity.java
@@ -1,15 +1,20 @@
 package com.mxt.anitrend.view.activity.base;
 
+import android.Manifest;
 import android.os.Bundle;
 
 import com.mxt.anitrend.R;
 import com.mxt.anitrend.base.custom.activity.ActivityBase;
 import com.mxt.anitrend.presenter.base.BasePresenter;
 
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -19,31 +24,45 @@ public class ReportActivity extends ActivityBase<Void, BasePresenter> {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_test);
+        StringBuilder log = new StringBuilder();
         try {
             Process process = Runtime.getRuntime().exec("logcat -d");
             BufferedReader bufferedReader = new BufferedReader(
                     new InputStreamReader(process.getInputStream()));
 
-            StringBuilder log = new StringBuilder();
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 log.append(line).append("\n");
             }
-            ((TextView)findViewById(R.id.report_display)).setText(log.toString());
+            ((TextView) findViewById(R.id.report_display)).setText(log.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+        Button saveLogcat = findViewById(R.id.save_logcat_button);
+        saveLogcat.setOnClickListener(view -> {
+            if (requestPermissionIfMissing(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                try {
+                    File root = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS),
+                            "AniTrend Logcat.txt");
+                    FileWriter writer = new FileWriter(root);
+                    writer.append(log.toString());
+                    writer.flush();
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }});
+        }
 
-    @Override
-    protected void onActivityReady() {
-    }
+        @Override
+        protected void onActivityReady () {
+        }
 
-    @Override
-    protected void updateUI() {
-    }
+        @Override
+        protected void updateUI () {
+        }
 
-    @Override
-    protected void makeRequest() {
+        @Override
+        protected void makeRequest () {
+        }
     }
-}
