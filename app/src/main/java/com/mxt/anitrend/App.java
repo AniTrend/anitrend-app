9935c2092351420a90c7d75834e036e0dcfe9b69
Maxwell <mxt.developer@gmail.com>
2019-01-21 22:49:07 +0200

    Temporary Workaround for Patching TLS Support #102
    - More details can be found here https://developer.android.com/training/articles/security-gms-provider

--- app/src/main/java/com/mxt/anitrend/App.java
+++ app/src/main/java/com/mxt/anitrend/App.java
@@ -1,12 +1,15 @@
 package com.mxt.anitrend;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.multidex.MultiDex;
 import android.support.multidex.MultiDexApplication;
 
 import com.crashlytics.android.core.CrashlyticsCore;
+import com.google.android.gms.security.ProviderInstaller;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.mxt.anitrend.model.entity.MyObjectBox;
 import com.mxt.anitrend.util.ApplicationPref;
@@ -63,6 +66,19 @@ public class App extends MultiDexApplication {
             analytics.setMinimumSessionDuration(5000L);
         }
         JobSchedulerUtil.scheduleJob(getApplicationContext());
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP)
+            ProviderInstaller.installIfNeededAsync(getApplicationContext(),
+                    new ProviderInstaller.ProviderInstallListener() {
+                        @Override
+                        public void onProviderInstalled() {
+
+                        }
+
+                        @Override
+                        public void onProviderInstallFailed(int i, Intent intent) {
+
+                        }
+                    });
     }
 
     @Override
