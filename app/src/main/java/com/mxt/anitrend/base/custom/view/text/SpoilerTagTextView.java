9cb1f909f75c6dddefe1199b157338929cb8a60a
Maxwell Mapako <wax911@users.noreply.github.com>
2019-02-26 12:55:16 +0200

    Update SpoilerTagTextView.java
    __Changes__
    - No need to override the `onInit()` method in the class since the base implementation already configures the view for single line behaviour.
    - No need to also call `onInit()` in the constructors of the child class as the `super` constructors already do that
    - For the sake of compatibility we'll use `CompatUtil.getColor` to avoid pre-lollipop exception that might be thrown

--- app/src/main/java/com/mxt/anitrend/base/custom/view/text/SpoilerTagTextView.java
+++ app/src/main/java/com/mxt/anitrend/base/custom/view/text/SpoilerTagTextView.java
@@ -11,38 +11,18 @@ public class SpoilerTagTextView extends SingleLineTextView {
 
     public SpoilerTagTextView(Context context) {
         super(context);
-        onInit();
     }
 
     public SpoilerTagTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        onInit();
     }
 
     public SpoilerTagTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        onInit();
     }
 
     @BindingAdapter("app:isSpoiler")
     public static void setIsSpoiler(SingleLineTextView view, Boolean isSpoiler) {
-        if (isSpoiler) view.setTextColor(view.getResources().getColor(R.color.colorStateOrange));
-    }
-
-    /**
-     * Clean up any resources that won't be needed
-     */
-    @Override
-    public void onViewRecycled() {
-
-    }
-
-    /**
-     * Optionally included when constructing custom views
-     */
-    @Override
-    public void onInit() {
-        setSingleLine(true);
-        setEllipsize(TextUtils.TruncateAt.END);
+        if (isSpoiler) view.setTextColor(CompatUtil.getColor(view.getContext(), R.color.colorStateOrange));
     }
 }
