0dd10854d54c5ddaf97e659ffe8c8ccdd434900f
Maxwell <mxt.developer@gmail.com>
2020-03-30 12:03:30 +0200

    Move getString extension functions to KoinExt object

--- app/src/main/java/com/mxt/anitrend/extension/KoinExt.kt
+++ app/src/main/java/com/mxt/anitrend/extension/KoinExt.kt
@@ -1,6 +1,9 @@
 package com.mxt.anitrend.extension
 
+import android.content.Context
+import androidx.annotation.StringRes
 import org.koin.core.KoinComponent
+import org.koin.core.get
 
 object KoinExt : KoinComponent {
 
@@ -13,4 +16,18 @@ object KoinExt : KoinComponent {
     fun <T : Any> get(`class`: Class<T>): T {
         return getKoin().get(`class`.kotlin, null, null)
     }
+
+    fun getString(@StringRes text: Int): String? =
+        runCatching {
+            get<Context>().getString(text)
+        }.also {
+            it.exceptionOrNull()?.printStackTrace()
+        }.getOrNull()
+
+    fun getString(@StringRes text: Int, vararg values: String): String? =
+        runCatching {
+            get<Context>().getString(text, *values)
+        }.also {
+            it.exceptionOrNull()?.printStackTrace()
+        }.getOrNull()
 }
