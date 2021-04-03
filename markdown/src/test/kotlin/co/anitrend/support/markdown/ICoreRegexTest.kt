b8a0341ff23b27382fcb89f6072e6445347394ff
Maxwell <mxt.developer@gmail.com>
2021-04-04 00:23:13 +0200

    Remove markdown module

--- markdown/src/test/kotlin/co/anitrend/support/markdown/ICoreRegexTest.kt
+++
@@ -1,9 +0,0 @@
-package co.anitrend.support.markdown
-
-import co.anitrend.support.markdown.core.contract.IMarkdownPlugin
-
-interface ICoreRegexTest {
-    val plugin: IMarkdownPlugin
-
-    fun `defined regex pattern detect elements`()
-}
