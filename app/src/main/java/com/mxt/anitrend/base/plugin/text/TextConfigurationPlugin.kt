dec22f383e3b25e6bedb30efb6f05d8921a267a6
Maxwell <mxt.developer@gmail.com>
2021-04-05 09:59:00 +0200

    Check for video link clicks

--- app/src/main/java/com/mxt/anitrend/base/plugin/text/TextConfigurationPlugin.kt
+++ app/src/main/java/com/mxt/anitrend/base/plugin/text/TextConfigurationPlugin.kt
@@ -1,13 +1,19 @@
 package com.mxt.anitrend.base.plugin.text
 
+import android.content.Intent
 import android.view.View
+import android.webkit.MimeTypeMap
 import androidx.core.net.toUri
 import androidx.fragment.app.FragmentActivity
 import com.mxt.anitrend.R
+import com.mxt.anitrend.util.KeyUtil
+import com.mxt.anitrend.view.activity.base.VideoPlayerActivity
 import com.mxt.anitrend.view.sheet.BottomSheetSpoiler
 import io.noties.markwon.*
 
 internal class TextConfigurationPlugin private constructor() : AbstractMarkwonPlugin() {
+
+    private val regex = Regex("mp4|webm", RegexOption.IGNORE_CASE)
 
     private fun decodeDataIn(link: String): String? {
         val uri = link.toUri()
@@ -27,7 +33,14 @@ internal class TextConfigurationPlugin private constructor() : AbstractMarkwonPl
                         sheet.show(context.supportFragmentManager, sheet.TAG)
                     }
                 } else {
-                    super.resolve(view, link)
+                    val mediaType = MimeTypeMap.getFileExtensionFromUrl(link)
+                    if (mediaType.matches(regex)) {
+                        val intent = Intent(view.context, VideoPlayerActivity::class.java)
+                        intent.putExtra(KeyUtil.arg_model, link)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        view.context.startActivity(intent)
+                    } else
+                        super.resolve(view, link)
                 }
             }
         })
