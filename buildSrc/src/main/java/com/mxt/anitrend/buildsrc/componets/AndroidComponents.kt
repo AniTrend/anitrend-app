8ef50f2161c5b3e420427b002fa7b2214fffe293
Maxwell <mxt.developer@gmail.com>
2021-04-13 15:52:25 +0200

    Apply signing configuration if a keystore file exists

--- buildSrc/src/main/java/com/mxt/anitrend/buildsrc/componets/AndroidComponents.kt
+++ buildSrc/src/main/java/com/mxt/anitrend/buildsrc/componets/AndroidComponents.kt
@@ -43,6 +43,18 @@ private fun Properties.applyToBuildConfigForBuild(buildType: BuildType) {
     }
 }
 
+private fun Project.signingConfiguration(properties: Properties) {
+    baseExtension().signingConfigs {
+        getByName("release") {
+            storeFile(file(properties["STORE_FILE"] as String))
+            storePassword(properties["STORE_PASSWORD"] as String)
+            keyAlias(properties["STORE_KEY_ALIAS"] as String)
+            keyPassword(properties["STORE_KEY_PASSWORD"] as String)
+            isV2SigningEnabled = true
+        }
+    }
+}
+
 private fun NamedDomainObjectContainer<BuildType>.applyConfiguration(project: Project) {
     asMap.forEach { buildTypeEntry ->
         println("Configuring build type -> ${buildTypeEntry.key}")
@@ -56,6 +68,16 @@ private fun NamedDomainObjectContainer<BuildType>.applyConfiguration(project: Pr
                 }
             }
         else println("${secretsFile.absolutePath} could not be found, build may fail")
+
+
+        val keyStoreFile = project.file(".config/keystore.properties")
+        if (keyStoreFile.exists())
+            secretsFile.inputStream().use { fis ->
+                Properties().run {
+                    load(fis); project.signingConfiguration(this)
+                }
+            }
+        else println("${keyStoreFile.absolutePath} could not be found, release may fail")
 
         val configurationFile = project.file(".config/configuration.properties")
         if (configurationFile.exists())
