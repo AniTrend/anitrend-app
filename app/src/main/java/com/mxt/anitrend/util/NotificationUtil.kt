2e91a1bc86069d23912f4c3a4cb8260419061e4a
LuK1337 <priv.luk@gmail.com>
2022-10-10 18:05:10 +0200

    Use PendingIntent.FLAG_IMMUTABLE flag for our intents
    Required since we target Android 12+ API:
    https://developer.android.com/guide/components/intents-filters#DeclareMutabilityPendingIntent

--- app/src/main/java/com/mxt/anitrend/util/NotificationUtil.kt
+++ app/src/main/java/com/mxt/anitrend/util/NotificationUtil.kt
@@ -44,7 +44,7 @@ class NotificationUtil(
                 context,
                 defaultNotificationId,
                 targetActivity,
-                PendingIntent.FLAG_UPDATE_CURRENT
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
     }
 
@@ -57,7 +57,7 @@ class NotificationUtil(
             context,
             action.hashCode(),
             intent,
-            PendingIntent.FLAG_UPDATE_CURRENT
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
     }
 
