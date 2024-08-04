499e73fc1e7e2ed71e06368acef168d49a80ee2c
Maxwell <mxt.developer@gmail.com>
2024-08-04 12:09:11 +0200

    chore: bump compile & target SDK from 33 to 34

--- buildSrc/src/main/java/com/mxt/anitrend/buildsrc/common/Configuration.kt
+++ buildSrc/src/main/java/com/mxt/anitrend/buildsrc/common/Configuration.kt
@@ -19,7 +19,7 @@ package com.mxt.anitrend.buildsrc.common
 
 object Configuration {
 
-    const val compileSdk = 33
-    const val targetSdk = 33
+    const val compileSdk = 34
+    const val targetSdk = 34
     const val minSdk = 21
 }
